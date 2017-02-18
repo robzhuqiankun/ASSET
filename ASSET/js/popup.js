0f5515515ce91cffa1746cb3d33f3e6563131bc3
zhuqiankun70 <robzhuqiankun@gmail.com>
2017-02-18 12:05:58 +0800

    init

---
+++ ASSET/js/popup.js
@@ -0,0 +1,30 @@
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var site_url = document.getElementById("site_url");
+    var full_url = tabs[0].url;
+    var host = full_url.replace(/\w*:\/\/([^\/]*)\/.*/, "$1");
+    site_url.value = host;
+    var submit_button = document.getElementById("submit");
+    submit_button.onclick=function(){
+        var keyword = document.getElementById("input_keyword").value;
+        var baidu = document.getElementById("use_baidu");
+        if (baidu.checked) {
+            window.open("http://www.baidu.com/s?wd=site:("+ host +")+" + keyword);
+        }
+        
+        var google = document.getElementById("use_google");
+        if (google.checked) {
+            window.open("http://www.google.com/search?safe=off&q=site:"+ host +"+" + keyword);
+        }
+
+        var bing = document.getElementById("use_bing");
+        if (bing.checked) {
+            window.open("http://www.bing.com/search?q=site:" + host + "+" + keyword);
+        }
+
+        var sogou = document.getElementById("use_sogou");
+        if (sogou.checked) {
+            window.open("https://www.sogou.com/web?query=site:" + host + "+" + keyword);
+        }
+    };
+});
+
